19f32126be1b6b8971f428aff8293031cb319d89
GaboYR <gabriel.yarleque.r@uni.pe>
2024-09-13 12:21:54 -0500

    Rename files

--- get_first_frame.py
+++ get_first_frame.py
@@ -2,14 +2,14 @@ import cv2
 
 # Abrimos el video
 
-video = cv2.VideoCapture('video_1_tarde.mp4')
+video = cv2.VideoCapture('video_1_temprano.mp4')
 
 # Leemos el primer frame
 check, img = video.read()
 
 if check:
     # Guardamos el primer frame
-    cv2.imwrite('primer_frame.png', img)
+    cv2.imwrite('fondo.png', img)
     print('Primer frame guardado')
 else:
     print('No se pudo leer el primer frame')
